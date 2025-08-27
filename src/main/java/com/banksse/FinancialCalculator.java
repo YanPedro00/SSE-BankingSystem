8b0851809a34033233155a57a9439ccc7b29b5a0
andre-gomes-silva <andre.gomesdasilva@thoughtworks.com>
2025-08-27 14:43:10 -0300

    feat: implement methods to Financial Calculator class to calculate the discount and the Discount applied

--- src/main/java/com/banksse/FinancialCalculator.java
+++ src/main/java/com/banksse/FinancialCalculator.java
@@ -7,7 +7,7 @@ class FinancialCalculator {
     public BigDecimal simpleInterest(BigDecimal principal, BigDecimal rate, int years)
     {
         BigDecimal interest = principal.multiply(rate).multiply(BigDecimal.valueOf(years));
-        interest = interest.setScale(2, RoundingMode.HALF_EVEN);
+        interest = interest.setScale(2, RoundingMode.HALF_UP);
         return interest;
     }
 
@@ -21,9 +21,7 @@ class FinancialCalculator {
         BigDecimal onePlusRate = BigDecimal.ONE.add(rate);
         BigDecimal factor = onePlusRate.pow(years);
         BigDecimal amount = principal.multiply(factor);
-
-
-        amount = amount.setScale(2, RoundingMode.HALF_EVEN);
+        amount = amount.setScale(2, RoundingMode.HALF_UP);
         BigDecimal interest = amount.subtract(principal);
         return interest;
     }
@@ -32,5 +30,18 @@ class FinancialCalculator {
         BigDecimal interest = compoundInterest(principal, rate, years);
         BigDecimal amount = principal.add(interest);
         return amount;
+    }
+
+    public BigDecimal discount(BigDecimal futureValue, BigDecimal rate, int years) {
+        BigDecimal discount = futureValue.multiply(rate).multiply(BigDecimal.valueOf(years));
+        discount = discount.setScale(2, RoundingMode.HALF_UP);
+        return discount;
+    }
+
+    public BigDecimal applyDiscount(BigDecimal futureValue, BigDecimal rate, int years) {
+        BigDecimal discountValue = discount(futureValue, rate, years);
+        BigDecimal discountApplied = futureValue.subtract(discountValue);
+        discountApplied = discountApplied.setScale(2, RoundingMode.HALF_UP);
+        return discountApplied;
     }
 }
